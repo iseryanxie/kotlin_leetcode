9272d3a61c0bc5a03cc7cc696702c2c11ae06327
Ryan Xie <ryan.xie@doordash.com>
2021-09-19 09:54:39 -0700

    [LC 0007] edit comments

--- src/main/kotlin/com/ryanxie/leetcode/Solution0001.kt
+++ src/main/kotlin/com/ryanxie/leetcode/Solution0001.kt
@@ -1,10 +1,10 @@
 package com.ryanxie.leetcode
 
+/**
+ * Write your thoughts here
+ */
 class Solution0001 {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        // not used
-        // val nNums = nums.size
-
         val sumToIdx = HashMap<Int, Int>()
 
         for ((idx, num) in nums.withIndex()) {
