a644300bcf5e971c59f921823b247806d1d6da5d
Ryan Xie <ryan.xie@doordash.com>
2021-09-19 09:51:42 -0700

    [LC 0007] reformat and naming convention

--- src/main/kotlin/com/ryanxie/leetcode/Solution0007.kt
+++ src/main/kotlin/com/ryanxie/leetcode/Solution0007.kt
@@ -3,13 +3,13 @@ package com.ryanxie.leetcode
 class Solution0007 {
     fun reverse(x: Int): Int {
         val negFlag = x < 0
-        var x_pos = if (negFlag) -x else x
+        var xPos = if (negFlag) -x else x
         var ans: Long = 0
         var cur: Int
-        while (x_pos > 0) {
-            cur = x_pos % 10
+        while (xPos > 0) {
+            cur = xPos % 10
             ans = ans * 10 + cur
-            x_pos = x_pos / 10
+            xPos /= 10
         }
         val ansInt = try {
             if (ans < Int.MAX_VALUE) ans.toInt() else 0
