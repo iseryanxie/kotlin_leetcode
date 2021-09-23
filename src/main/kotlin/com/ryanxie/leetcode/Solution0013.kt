841f32ecea443014b2949f91042d4cd3c61844dc
Ryan Xie <ryan.xie@doordash.com>
2021-09-23 05:18:25 -0700

    [LC 13] edit - change initialize method of hashmap and comment

--- src/main/kotlin/com/ryanxie/leetcode/Solution0013.kt
+++ src/main/kotlin/com/ryanxie/leetcode/Solution0013.kt
@@ -5,14 +5,26 @@ package com.ryanxie.leetcode
  */
 class Solution0013 {
     fun romanToInt(s: String): Int {
-        val numMap = hashMapOf<Char, Int>()
-        numMap['I'] = 1
-        numMap['V'] = 5
-        numMap['X'] = 10
-        numMap['L'] = 50
-        numMap['C'] = 100
-        numMap['D'] = 500
-        numMap['M'] = 1000
+        val numMap = hashMapOf<Char, Int>(
+            'I' to 1,
+            'V' to 5,
+            'X' to 10,
+            'L' to 50,
+            'C' to 100,
+            'D' to 500,
+            'M' to 1000
+        )
+//        val numMap = hashMapOf<Char,Int>()
+//        numMap['I'] = 1
+//        numMap['V'] = 5
+//        numMap['X'] = 10
+//        numMap['L'] = 50
+//        numMap['C'] = 100
+//        numMap['D'] = 500
+//        numMap['M'] = 1000
+        // It is interesting that you can define an empty val hashmap
+        // then initialize it. That changes the values in hashmap!!
+
         var prev = 0
         var sum = 0
         s.forEach {
