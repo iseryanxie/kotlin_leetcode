2b9e309c06bf5a53cb2b3ed33a57af5da4108979
Ryan Xie <ryan.xie@doordash.com>
2021-10-03 10:21:06 -0700

    [LC 28] reformat

--- src/main/kotlin/com/ryanxie/leetcode/Solution0028.kt
+++ src/main/kotlin/com/ryanxie/leetcode/Solution0028.kt
@@ -2,18 +2,20 @@ package com.ryanxie.leetcode
 
 /**
  * Write your thoughts here
+ * substring will throw exception if out of bound
+ *
  */
 class Solution0028 {
     fun strStr(haystack: String, needle: String): Int {
-        if (needle.length ==0) {
+        if (needle.isEmpty()) {
             return 0
         }
-        for (i in haystack.indices){
-            if (haystack[i]==needle[0]){
-                if (i+needle.length>haystack.length){
+        for (i in haystack.indices) {
+            if (haystack[i] == needle[0]) {
+                if (i + needle.length > haystack.length) {
                     return -1
                 }
-                if (haystack.substring(i,i+needle.length) == needle){
+                if (haystack.substring(i, i + needle.length) == needle) {
                     return i
                 }
             }
