d478d1af301e8de421f990802849c953c9f1cd26
Ryan Xie <ryan.xie@doordash.com>
2022-03-05 21:55:20 -0800

    [LC 0098] add

--- src/main/kotlin/com/ryanxie/leetcode/Solution0098.kt
+++ src/main/kotlin/com/ryanxie/leetcode/Solution0098.kt
@@ -2,6 +2,8 @@ package com.ryanxie.leetcode
 
 /**
  * Write your thoughts here
+ * Not only left and right subtrees are BST, but also any node in left subtree must be less than the current node,
+ * and any node in right subtree must be greater than the current node.
  */
 class Solution0098 {
     fun isValidBSTMinMax(root: TreeNode?, minValue: Long, maxValue: Long): Boolean {
