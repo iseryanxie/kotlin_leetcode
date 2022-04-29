876cd7fd4dd7c77114e8ac27d06def6608610f47
Ryan Xie <ryan.xie@doordash.com>
2022-04-28 21:42:32 -0700

    [add] LC162

--- src/main/kotlin/com/ryanxie/leetcode/Solution0162.kt
+++ src/main/kotlin/com/ryanxie/leetcode/Solution0162.kt
@@ -13,12 +13,14 @@ class Solution0162 {
         while (left < right) {
             val mid = left + (right - left) / 2
             if (nums[mid] > nums[mid + 1]) {
-                // must have a peak on the left
+                // must have a peak on the left, mid is greater, so include mid in the search
                 right = mid
             } else {
+                // must have a peak on the right, mid+1 is greater, so include mid+1 in the search
                 left = mid + 1
             }
         }
+        // left == right at this point
         return left
     }
 }
