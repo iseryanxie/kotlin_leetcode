b882ba9ea49c25b4579216b0bee0f01a992263ce
Ryan Xie <ryan.xie@doordash.com>
2022-07-08 22:49:23 -0700

    [add] LC227

---
+++ src/main/kotlin/com/ryanxie/leetcode/Solution0227.kt
@@ -0,0 +1,55 @@
+package com.ryanxie.leetcode
+
+import java.util.*
+
+/**
+ * Write your thoughts here
+ */
+class Solution0227 {
+    fun calculate(s: String): Int {
+        var current = 0
+        var preSign = -1
+        val stack = Stack<Int>()
+
+        s.forEach{ c->
+            if(c.isDigit()){
+                current = 10*current+ (c-'0')
+            } else if(c != ' '){
+
+                addNum(stack, current, preSign)
+
+                if(c == '+'){
+                    preSign = 0
+                } else if(c == '-'){
+                    preSign = 1
+                } else if(c == '*'){
+                    preSign = 2
+                } else if(c == '/'){
+                    preSign = 3
+                }
+                current = 0
+            }
+        }
+
+        addNum(stack, current, preSign)
+
+        var res = 0
+        stack.forEach{
+            res+=it
+        }
+        return res
+
+    }
+
+    fun addNum(stack:Stack<Int>, current:Int, preSign: Int){
+        if(preSign == 0 || preSign == -1){
+            stack.push(current)
+        } else if(preSign == 1){
+            stack.push(-current)
+        } else if(preSign == 2){
+            stack.push(stack.pop()*current)
+        } else if(preSign == 3){
+            stack.push(stack.pop()/current)
+        }
+    }
+}
---
+++ src/test/kotlin/com/ryanxie/leetcode/Solution0227Test.kt
@@ -0,0 +1,27 @@
+package com.ryanxie.leetcode
+
+import org.junit.Assert
+import org.junit.Test
+
+class Solution0227Test{
+    private val solution = Solution0227()
+
+    @Test
+    fun test1(){
+        val actual = solution.calculate(" 3/2 ")
+        Assert.assertEquals(1, actual)
+    }
+
+    @Test
+    fun test2(){
+        val actual = solution.calculate("1")
+        Assert.assertEquals(1, actual)
+    }
+
+    @Test
+    fun test3(){
+        val actual = solution.calculate("3+2*2")
+        Assert.assertEquals(7, actual)
+    }
+
+}
