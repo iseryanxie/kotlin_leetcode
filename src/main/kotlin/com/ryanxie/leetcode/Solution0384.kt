b8c6bdb45304b9e8ef931b40bf657e8a8bf8d347
Ryan Xie <ryan.xie@doordash.com>
2022-08-20 18:01:32 -0700

    [LC 384]

--- src/main/kotlin/com/ryanxie/leetcode/Solution0384.kt
+++ src/main/kotlin/com/ryanxie/leetcode/Solution0384.kt
@@ -12,7 +12,9 @@ class Solution0384 (val nums: IntArray) {
     fun shuffle(): IntArray {
         val result = nums.clone()
         for (i in nums.indices) {
+            // randomly pick next index in the remaining indexes
             val j = (Math.random() * (nums.size - i)).toInt() + i
+            // swap the two elements
             val temp = result[i]
             result[i] = result[j]
             result[j] = temp
