77b31ebcb8ed005ba9ef87af3f6901fa1b0285fe
Ryan Xie <ryan.xie@doordash.com>
2023-05-16 22:28:52 -0700

    [LC450]

---
+++ src/main/kotlin/com/ryanxie/leetcode/Solution0450.kt
@@ -0,0 +1,39 @@
+package com.ryanxie.leetcode
+
+/**
+ * Write your thoughts here
+ */
+class Solution0450 {
+    fun deleteNode(root: TreeNode?, key: Int): TreeNode? {
+        if (root == null) {
+            return null
+        }
+        if (root.`val` == key) {
+            // root is the key, need to delete root
+            if (root.left == null) {
+                return root.right
+            }
+            if (root.right == null) {
+                return root.left
+            }
+            // both left and right subtree exist, find the min node in right subtree to replace root
+            val minNode = findMin(root.right)
+            root.`val` = minNode.`val`
+            root.right = deleteNode(root.right, minNode.`val`)
+        } else if (root.`val` > key) {
+            // key is in left subtree
+            root.left = deleteNode(root.left, key)
+        } else {
+            // key is in right subtree
+            root.right = deleteNode(root.right, key)
+        }
+        return root
+    }
+    private fun findMin(root: TreeNode?): TreeNode {
+        var node = root
+        while (node?.left != null) {
+            node = node.left
+        }
+        return node!!
+    }
+}
---
+++ src/test/kotlin/com/ryanxie/leetcode/Solution0450Test.kt
@@ -0,0 +1,34 @@
+package com.ryanxie.leetcode
+
+import org.junit.Assert
+import org.junit.Test
+
+class Solution0450Test{
+    private val solution = Solution0450()
+
+    @Test
+    fun test1(){
+        val actual = solution.deleteNode(TreeNode(5).also {
+            it.left = TreeNode(3).also {
+                it.left = TreeNode(2)
+                it.right = TreeNode(4)
+            }
+            it.right = TreeNode(6).also {
+                it.right = TreeNode(7)
+            }
+        }, 3)
+        val expected = TreeNode(5).also {
+            it.left = TreeNode(4).also {
+                it.left = TreeNode(2)
+            }
+            it.right = TreeNode(6).also {
+                it.right = TreeNode(7)
+            }
+        }
+        Assert.assertEquals(expected.`val`, actual?.`val`)
+        Assert.assertEquals(expected.left?.`val`, actual?.left?.`val`)
+        Assert.assertEquals(expected.left?.left?.`val`, actual?.left?.left?.`val`)
+        Assert.assertEquals(expected.right?.`val`, actual?.right?.`val`)
+        Assert.assertEquals(expected.right?.right?.`val`, actual?.right?.right?.`val`)
+    }
+}
