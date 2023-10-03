298c86fcc19086685d303d71c5723a0df041a864
Ryan Xie <ryan.xie@doordash.com>
2023-10-02 23:33:10 -0700

    comments

--- src/main/kotlin/com/ryanxie/leetcode/Solution0164.kt
+++ src/main/kotlin/com/ryanxie/leetcode/Solution0164.kt
@@ -4,8 +4,9 @@ package com.ryanxie.leetcode
  * Write your thoughts here
  * Pigeonhole principle
  * https://en.wikipedia.org/wiki/Pigeonhole_principle
- * The maximum gap will be no smaller than ceiling[(max - min ) / (N - 1)]
- * So the maximum gap can only be found in the gaps between buckets
+ * 1. all the elements within the bucket will have a gap < bucketSize (bucketSize = (max - min) / (n - 1))
+ * 2. we created N-1 buckets for N - 2 elements, so at least one bucket is empty, so the max gap will come from
+ * the numbers lying in the buckets to the left and right of the empty bucket
  */
 class Solution0164 {
     fun maximumGap(nums: IntArray): Int {
@@ -14,7 +15,7 @@ class Solution0164 {
         }
         val min = nums.minOrNull()!!
         val max = nums.maxOrNull()!!
-        // determine the size of bucket
+        // determine the size of bucket, the gap of numbers within the same bucket is smaller than bucketSize
         val bucketSize = maxOf(1, (max - min) / (nums.size - 1))
         // based on the size of bucket, determine the max number of buckets
         val bucketCount = (max - min) / bucketSize + 1
