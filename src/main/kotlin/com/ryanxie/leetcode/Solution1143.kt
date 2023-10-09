099f43f0acd40d9552a64ecba8f0f78ad227a593
Ryan Xie <ryan.xie@doordash.com>
2023-10-08 21:51:52 -0700

    [LC 516] [LC1143]

--- src/main/kotlin/com/ryanxie/leetcode/Solution1143.kt
+++ src/main/kotlin/com/ryanxie/leetcode/Solution1143.kt
@@ -8,12 +8,15 @@ class Solution1143 {
     fun longestCommonSubsequence(text1: String, text2: String): Int {
         val m = text1.length
         val n = text2.length
+        // dp[i][j] is the longest common subsequence of text1[0..i-1] and text2[0..j-1]
         val dp = Array(m + 1) { IntArray(n + 1) }
         for (m in 1..m) {
             for (n in 1..n) {
                 dp[m][n] = if (text1[m - 1] == text2[n - 1]) {
+                    // if there is a common char, then add 1 then solve the subproblem of moving both pointers
                     dp[m - 1][n - 1] + 1
                 } else {
+                    // not match, it is the max of moving either pointer
                     maxOf(dp[m - 1][n], dp[m][n - 1])
                 }
             }
